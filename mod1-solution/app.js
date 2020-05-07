aa6ad228a26026d0fb5df3b22b60830b082f9feb
Neha <nehajagtap27@gmail.com>
2020-05-07 14:49:21 +0530

    new folder

---
+++ mod1-solution/app.js
@@ -0,0 +1,49 @@
+(function() {
+  'use strict';
+
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController',LunchCheckController);
+
+//   .controller('LunchCheckController',function($scope){
+//     $scope.menu="nehaha";
+//     $scope.message="";
+//     $scope.check=function(){
+//       var items= $scope.menu.length();
+//       var msg=$scope.message;
+//       if(items<=3)
+//       {
+//         msg="Enjoy!";
+//         return msg;
+//       }
+//       if(items>3)
+//       {
+//         msg="Too much!";
+//         return msg;
+//       }
+//
+//   }
+// });
+
+  LunchCheckController.$inject=['$scope'];
+  function LunchCheckController($scope)
+  {
+    $scope.menu="nehaha";
+    $scope.message="";
+  //   $scope.check=function(){
+  //     var items= $scope.menu.length();
+  //     var msg=$scope.message;
+  //     if(items<=3)
+  //     {
+  //       msg="Enjoy!";
+  //       return msg;
+  //     }
+  //     if(items>3)
+  //     {
+  //       msg="Too much!";
+  //       return msg;
+  //     }
+  //
+  //   }
+  }
+
+  });
