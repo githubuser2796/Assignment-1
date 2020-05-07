925d37a3fe71b8d7e9627bea1126767938ec7a15
Neha <nehajagtap27@gmail.com>
2020-05-07 15:31:28 +0530

    new folder

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -2,7 +2,31 @@
   'use strict';
 
   angular.module('LunchCheck',[])
-  .controller('LunchCheckController',function($scope){
+  .controller('LunchCheckController',LunchCheckController);
+
+//   .controller('LunchCheckController',function($scope){
+//     $scope.menu="nehaha";
+//     $scope.message="";
+//     $scope.check=function(){
+//       var items= $scope.menu.length();
+//       var msg=$scope.message;
+//       if(items<=3)
+//       {
+//         msg="Enjoy!";
+//         return msg;
+//       }
+//       if(items>3)
+//       {
+//         msg="Too much!";
+//         return msg;
+//       }
+//
+//   }
+// });
+
+  LunchCheckController.$inject=['$scope','$filter'];
+  function LunchCheckController($scope,$filter)
+  {
     $scope.menu="nehaha";
     $scope.message="";
     $scope.check=function(){
@@ -19,29 +43,7 @@
         return msg;
       }
 
+    }
   }
-});
 
-  // LunchCheckController.$inject=['$scope'];
-  // function LunchCheckController($scope)
-  // {
-  //   $scope.menu="nehaha";
-  //   $scope.message="";
-  //   $scope.check=function(){
-  //     var items= $scope.menu.length();
-  //     var msg=$scope.message;
-  //     if(items<=3)
-  //     {
-  //       msg="Enjoy!";
-  //       return msg;
-  //     }
-  //     if(items>3)
-  //     {
-  //       msg="Too much!";
-  //       return msg;
-  //     }
-  //
-  //   }
-  // }
-
-  });
+})();
